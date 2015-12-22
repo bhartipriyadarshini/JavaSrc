43a3afd30e17cf3889dbfc4cefdade96c3d19b24
Bharti Priyadarshini <bharti.priyadarshini@yahoo.com>
2015-12-22 18:38:55 +0530

    JDBC CONNECTION

---
+++ Sql Querries/DDL.sql
@@ -0,0 +1,3 @@
+alter table customer_Details modify gender varchar(20);
+Alter table customer_details add pet_name varchar2(20);
+Update customer_details set pet_name= 'nick' where cno= 'C01'; 
---
+++ Sql Querries/DML.sql
@@ -0,0 +1,4 @@
+insert into customer_details values('&cno','&cname','&CITY','&GENDER','&ADDRESS',&MOBILE);
+UPDATE customer_details set cno ='C05' where cno ='5';
+delete from customer_details where cno is null;
+
